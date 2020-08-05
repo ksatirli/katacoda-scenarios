0c07f91e1aa2d695139f817e43dec214845da77e
Kerim Satirli <k.satirli@gmail.com>
2020-08-05 23:13:25 +0200

    update

--- kubernetes-terraform-datadog/base.sh
+++ kubernetes-terraform-datadog/base.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# load configuration
-source config.sh
+TERRAFORM_VERSION="0.12.29"
+TERRAFORM_FILE="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+SCENARIO_WORKSPACE="/tmp/scenario"
+USER_WORKSPACE="/root/workspace"
 
 # install `unzip`
 apt-get \
--- kubernetes-terraform-datadog/step1.sh
+++ kubernetes-terraform-datadog/step1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# load configuration
-source config.sh
+SCENARIO_WORKSPACE="/tmp/scenario"
+USER_WORKSPACE="/root/workspace"
 
 # copy relevant files to workspace
 cp "${SCENARIO_WORKSPACE}/{terraform,variables}.tf" "${USER_WORKSPACE"
