2f2675c935e350f954e33bdf8d789f295d05fe3e
Taylor Dolezal <onlydole@gmail.com>
2020-08-06 16:52:12 -0700

    Add Datadog variables to variables file

--- k8s-terraform-datadog/step2/variables.tf
+++ k8s-terraform-datadog/step2/variables.tf
@@ -15,3 +15,13 @@ variable "application_name" {
   description = "Application Name"
   default     = "beacon"
 }
+
+variable "datadog_api_key" {
+  type        = string
+  description = "Datadog API Key"
+}
+
+variable "datadog_app_key" {
+  type        = string
+  description = "Datadog Application Key"
+}
