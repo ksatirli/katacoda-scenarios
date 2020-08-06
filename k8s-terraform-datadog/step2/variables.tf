ae650790a11aed83cf022520a0b1f1d20cb49d72
Taylor Dolezal <onlydole@gmail.com>
2020-08-06 16:53:15 -0700

    Forget you saw that

--- k8s-terraform-datadog/step2/variables.tf
+++ k8s-terraform-datadog/step2/variables.tf
@@ -15,13 +15,3 @@ variable "application_name" {
   description = "Application Name"
   default     = "beacon"
 }
-
-variable "datadog_api_key" {
-  type        = string
-  description = "Datadog API Key"
-}
-
-variable "datadog_app_key" {
-  type        = string
-  description = "Datadog Application Key"
-}
