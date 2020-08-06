f51293f56c48e0a9aefc749f3294fd70e27ba46a
Taylor Dolezal <onlydole@gmail.com>
2020-08-06 16:41:13 -0700

    Install Helm v3.x

--- k8s-terraform-datadog/intro/courseData.sh
+++ k8s-terraform-datadog/intro/courseData.sh
@@ -2,8 +2,6 @@
 
 TERRAFORM_VERSION="0.12.29"
 TERRAFORM_FILE="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-HELM_VERSION="v3.2.4"
-HELM_FILE="helm-${HELM_VERSION}-linux-amd64.tar.gz"
 SCENARIO_REPOSITORY="https://github.com/ksatirli/katacoda-scenarios.git"
 SCENARIO_WORKSPACE="/tmp/scenario"
 USER_WORKSPACE="/root/"
@@ -20,26 +18,22 @@ curl \
   --remote-name \
   "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_FILE}"
 
-# fetch Helm archive
-curl \
-  --remote-name \
-  "https://get.helm.sh/${HELM_VERSION}/${HELM_FILE}"
-
 # unzip Terraform archive and make it accessible in PATH
 unzip \
   "${TERRAFORM_FILE}" \
   -d "/usr/local/bin/"
 
-# unzip Helm archive and make it accessible in PATH
-tar -zxvf \
-  "${HELM_FILE}" \
-  -C "/usr/local/bin/"
-
 # clean up
 rm \
   --recursive \
   --force \
-  ${TERRAFORM_FILE} ${HELM_FILE}
+  ${TERRAFORM_FILE}
+
+# install Helm v3.x
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm get_helm.sh
 
 # create user workspace
 mkdir \
