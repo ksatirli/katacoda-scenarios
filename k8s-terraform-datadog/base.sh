6285f4e4825655a55cba0212d4597992ceab35ee
Kerim Satirli <k.satirli@gmail.com>
2020-08-06 22:50:17 +0200

    update

--- k8s-terraform-datadog/base.sh
+++ k8s-terraform-datadog/base.sh
@@ -42,3 +42,7 @@ git \
 && \
 cp \
   ${SCENARIO_WORKSPACE}/k8s-terraform-datadog/step1/*.tf "${USER_WORKSPACE}/"
+
+# make Datadog API credentials available
+export TF_VAR_datadog_api_key="${DD_API_KEY}"
+export TF_VAR_datadog_app_key="${DD_APP_KEY}"
