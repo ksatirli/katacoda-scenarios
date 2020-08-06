6a2c628bfbd17cf0fe7fc4109cb12543911abf11
Kerim Satirli <k.satirli@gmail.com>
2020-08-07 00:02:52 +0200

    update

--- k8s-terraform-datadog/step1/code.sh
+++ k8s-terraform-datadog/step1/code.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # make Datadog API credentials available
-export TF_VAR_datadog_api_key="db54e5221788b3492f9085a40a95766f${DD_API_KEY}"
-export TF_VAR_datadog_app_key="f0b7d947887cf1641a27ab9cc3ea0b2e2aeb5a7b${DD_APP_KEY}"
+export TF_VAR_datadog_api_key="${DD_API_KEY}"
+export TF_VAR_datadog_app_key="${DD_APP_KEY}"
