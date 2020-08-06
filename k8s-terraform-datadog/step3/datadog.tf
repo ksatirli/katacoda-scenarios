4b53fa2985f57bb8e7625d497adff8631e7ecf9a
Taylor Dolezal <onlydole@gmail.com>
2020-08-06 16:49:38 -0700

    Add Datadog API and App key stubs

--- k8s-terraform-datadog/step3/datadog.tf
+++ k8s-terraform-datadog/step3/datadog.tf
@@ -1,4 +1,6 @@
 # see https://www.terraform.io/docs/providers/datadog/index.html for more information
 provider "datadog" {
   # The values for `api_key` and `app_key` are populated from the environment
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
 }
