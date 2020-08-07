22f8c9fabbeb723a9fcad03a04fcd9ba77c2ee95
Kerim Satirli <k.satirli@gmail.com>
2020-08-07 15:42:48 +0200

    update

--- k8s-terraform-datadog/step3/variables_datadog.tf
+++ k8s-terraform-datadog/step3/variables_datadog.tf
@@ -1,17 +1,17 @@
 variable "datadog_user_email" {
-  type        = string
+  type = string
   description = "Email address for Datadog User"
-  default     = "<CHANGE ME>"
+  default = "<CHANGE ME>"
 }
 
 variable "datadog_api_key" {
-  type        = string
+  type = string
   description = "Datadog API Key"
   # the value for this variable is available via the Katacoda environment
 }
 
 variable "datadog_app_key" {
-  type        = string
+  type = string
   description = "Datadog App Key"
   # the value for this variable is available via the Katacoda environment
 }
