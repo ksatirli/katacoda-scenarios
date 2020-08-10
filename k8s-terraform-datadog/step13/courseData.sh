f06a97df718b7adb6e2a4a30edaba8cb0d6f61c3
Kerim Satirli <k.satirli@gmail.com>
2020-08-10 15:37:11 +0200

    linting

--- k8s-terraform-datadog/step13/courseData.sh
+++ k8s-terraform-datadog/step13/courseData.sh
@@ -4,7 +4,7 @@
 rm \
   --recursive \
   --force \
-  "/root/step13.tfplan"
+  "/root/step12.tfplan"
 
 # copy relevant files to workspace
 cp /tmp/scenario/k8s-terraform-datadog/step13/*.tf "/root/"
