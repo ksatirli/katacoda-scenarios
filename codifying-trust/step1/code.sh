ca3b9d0be7368db2b84a8fa1349136dc48a4fc8a
Kerim Satirli <k.satirli@gmail.com>
2021-04-25 15:47:37 +0200

    linting

--- codifying-trust/step1/code.sh
+++ codifying-trust/step1/code.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# change into workspace directory
+cd /workspace
+
 # helper function to allow easier copying of step-specific files
 kccopy() {
   cp /tmp/scenario/codifying-trust/step${1}/*.tf /workspace/
