528a8ec264e9d8fe93d1900e0cf54128cc880510
Kerim Satirli <k.satirli@gmail.com>
2021-04-25 14:50:42 +0200

    linting

--- codifying-trust/step2/terraform.tf
+++ codifying-trust/step2/terraform.tf
@@ -3,9 +3,9 @@ terraform {
     local = {
       # see https://registry.terraform.io/providers/hashicorp/local/latest
       source  = "hashicorp/local"
-      version = "2.1.0"
+
     }
   }
 
-  required_version = "0.15.0"
+  required_version = ""
 }
