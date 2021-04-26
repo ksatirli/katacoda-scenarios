c0143177a649ec1db1a34799d83eff7b7e152a7f
Tracy P Holmes <12778804+tracypholmes@users.noreply.github.com>
2021-04-26 03:53:38 -0500

    Updates Terraform version (#6)

--- k8s-terraform-datadog/intro/courseData.sh
+++ k8s-terraform-datadog/intro/courseData.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TERRAFORM_VERSION="0.12.29"
+TERRAFORM_VERSION="0.13.3"
 TERRAFORM_FILE="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 HELM_FILE=".helm-installer.sh"
 SCENARIO_REPOSITORY="https://github.com/ksatirli/katacoda-scenarios.git"
